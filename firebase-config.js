11a210e91a20f025ea0e4a595dc1b3d599f5bee8
Syed Taqi Shah Kazmi <syedtaqishahkazmi6@gmail.com>
2026-05-16 07:50:26 +0500

    add firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 window.firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDJD9PZUQuCjgzKWvxOVVY2rB1xzYmNkzw",
+  authDomain: "cooler-duty.firebaseapp.com",
+  databaseURL: "https://cooler-duty-default-rtdb.firebaseio.com",
+  projectId: "cooler-duty",
+  storageBucket: "cooler-duty.firebasestorage.app",
+  messagingSenderId: "535883747500",
+  appId: "1:535883747500:web:b1e9e7f69257df51a84f09",
 };
